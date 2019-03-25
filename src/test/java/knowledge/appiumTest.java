0e4978c58e8d0b354eb93ce8ca053119035e4485
kishore <mkishorekumarreddy89@gmail.com>
2019-03-25 11:18:43 +0530

    third commit

---
+++ src/test/java/knowledge/appiumTest.java
@@ -0,0 +1,18 @@
+package knowledge;
+
+import org.testng.annotations.Test;
+
+public class appiumTest {
+	
+	@Test
+	public void NativeAppAndroid()
+	{
+		System.out.println("NativeAppAndroid");
+	}
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");
+	}
+
+}
---
+++ src/test/java/knowledge/seleniumTest.java
@@ -0,0 +1,18 @@
+package knowledge;
+
+import org.testng.annotations.Test;
+
+public class seleniumTest {
+	
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+	}
+	@Test
+	public void ElementsUi()
+	{
+		System.out.println("ElementsUi");
+	}
+
+}
